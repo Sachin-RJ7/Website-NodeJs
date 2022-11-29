d18fc52002af9a2d9f6a35819b3061d74f524dc2
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-11-29 23:40:37 +0500

    | created schema for navbar |

---
+++ src/models/Detail.js
@@ -0,0 +1,14 @@
+// creating schema
+const mongoose = require('mongoose');
+const Detail = new mongoose.Schema({
+    brandName: String,
+    brandIconUrl: String,
+    links:[
+        {
+            label: String,
+            url: String,
+        },
+    ]
+});
+
+module.exports = mongoose.model('detail', Detail)

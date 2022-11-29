6af2b80d81670d5097893223482e85f9f528a794
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-11-29 23:33:02 +0500

    | added data for database and changes some lines |

--- src/app.js
+++ src/app.js
@@ -1,33 +1,67 @@
 // This is our main file....
-const express = require('express')
-const hbs = require('hbs')
-const mongoose = require('mongoose')
+const express = require("express");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
 const app = express();
+const Detail = require("./models/Detail"); // importing from Detail.js
 
-const routes = require('./routes/main');
+const routes = require("./routes/main");
 
-app.use('/static',express.static('public'))
-app.use('',routes);
+app.use("/static", express.static("public"));
+app.use("", routes);
 
 // (template engine)
-app.set('view engine', 'hbs')
-app.set('views','views')
-hbs.registerPartials('views/partials') // setting partials
-
+app.set("view engine", "hbs");
+app.set("views", "views");
+hbs.registerPartials("views/partials"); // setting partials
 
 // Database connection.....
-mongoose.connect('mongodb://localhost/website-_nodejs', ()=> {
-    console.log('db connected');
-    
+mongoose.connect("mongodb://0.0.0.0:27017/website_nodejs", (err) => {
+  if (err) {
+    console.log("unable to connect with the server");
+  }
+  else {
+    console.log("Mongodb is connected");
+  }
 })
 
+// data is storing in database...
+// Detail({
+//   brandName: "Sachin Kumar",
+//   brandIconUrl: "https://www.youtube.com/watch?v=8hFlOtBDNT0",
+//   links:[
+//     {
+//       label: "Home",
+//       url: "/"
+//     },
+//     {
+//       label: "Services",
+//       url: "/services"
+//     },
+//     {
+//       label: "Gallery",
+//       url: "/gallery"
+//     },
+//     {
+//       label: "About",
+//       url: "/about"
+//     },
+//     {
+//       label: "Contact",
+//       url: "/contact-us"
+//     },
+//   ]
+// }).save((err, db) => {
+//   if(err) {
+//     console.log("Error");
+//   }
+//   else {
+//     console.log(`User data saved successfully ${db}`);
+//   }
+// })
 
-// Now this will not execute because we are using now routes
-app.get('/', (request, response) => {
-    response.send("This data is from server");
-})
 
 // At this port, we are running our server
-app.listen(process.env.PORT | 5556,()=> {
-    console.log("Server started");
-})
+app.listen(process.env.PORT | 5000, () => {
+  console.log("Server started");
+});

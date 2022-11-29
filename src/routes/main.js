308429cef3e381dc0ae4f4f707d832784f56779e
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-11-29 23:35:34 +0500

    | added data for home and gallery page |

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const{route} = require('express/lib/application')
+
+const Detail = require("../models/Detail")
 
 const routes = express.Router();
 
-routes.get('/', (req, res)=> {
-    res.render('index');
-})
+routes.get('/', async (req, res)=> {
+    const details = await Detail.findOne({"_id":"638645a97b3bdc27c0adabfa"})
+    res.render('index',{
+        details:details
+    });
+});
 
-routes.get('/gallery', (req, res)=> {
-    res.render('gallery');
-})
+routes.get('/gallery', async (req, res)=> {
+    const details = await Detail.findOne({"_id":"638645a97b3bdc27c0adabfa"})
+    res.render('gallery',{
+        details:details
+    });
+});
+
 
 
 module.exports = routes;

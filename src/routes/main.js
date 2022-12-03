46581e970aa141f5820745a08a954e54549a0994
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-03 22:41:12 +0500

    | service section |

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,17 +2,20 @@ const express = require('express');
 const{route} = require('express/lib/application')
 
 const Detail = require("../models/Detail");
+const Service = require('../models/Service');
 const Slider = require('../models/Slider');
 
 const routes = express.Router();
 
 routes.get('/', async (req, res)=> {
     const details = await Detail.findOne({"_id":"638645a97b3bdc27c0adabfa"})
-    const slides = await Slider.find()
+    const slides = await Slider.find();
+    const services = await Service.find();
 
     res.render('index',{
         details:details,
-        slides:slides
+        slides:slides,
+        services:services
     });
 });
 
